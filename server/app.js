e9311d69ac2021f5afb80558cc1427cbb0e26bf5
wongalofonso <alfonsowongsd@gmail.com>
2018-02-14 20:25:08 -0800

    test

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 let uri = "mongodb://heroku_6ppd3vwr:5s5tqpnfbqd5kuv23s7rpg7ja9@ds235768.mlab.com:35768/heroku_6ppd3vwr";
 
-mongoose.connect(process.env.PORT || uri);
+mongoose.connect(process.env.MONGODB_URI || uri);
 
 mongoose.Promise = Promise;
 
