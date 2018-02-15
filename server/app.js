ac68cab00d07884adf0ea8c86c35fe53c17b85fb
wongalofonso <alfonsowongsd@gmail.com>
2018-02-14 19:15:49 -0800

    adds mlab

--- server/app.js
+++ server/app.js
@@ -3,10 +3,13 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
-mongoose.connect("mongodb://localhost/my-blog");
+let uri = process.env.MONGODB_URI ||"mongodb://heroku_6ppd3vwr:5s5tqpnfbqd5kuv23s7rpg7ja9@ds235768.mlab.com:35768/heroku_6ppd3vwr"
 
+mongoose.connect(uri);
 mongoose.Promise = Promise;
 
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 const app = express();
 
 app.use(morgan("dev"));
